106e2c79ee231de998a994d46dd13a8e3a05503c
ayoubtrabelsitr <ayoubtrabelsitr@gmail.com>
2024-09-26 14:16:16 +0200

    Vf0

--- fairDetection.py
+++ fairDetection.py
@@ -1,9 +1,10 @@
 from ultralytics import YOLO
-import cv2
+import os
 import matplotlib.pyplot as plt
 
 
 model = YOLO('ComputerVision/best.pt')
 
 
-results = model.predict(source=0, imgsz=640, conf=0.75, show=True)
+results = model.predict(source='ComputerVision/fire2.mp4', imgsz=640, conf=0.75, save=True)
+#results = model.predict(source=0, imgsz=640, conf=0.75, show=True)
